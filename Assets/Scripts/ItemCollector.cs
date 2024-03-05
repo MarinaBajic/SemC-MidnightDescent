42e0880d33ddd82248f007e6b59ddd8a1fef60bd
MarinaBajic <marinabajic10@gmail.com>
2024-03-05 22:26:53 +0100

    Smoother crates and cherries

--- Assets/Scripts/ItemCollector.cs
+++ Assets/Scripts/ItemCollector.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ItemCollector : MonoBehaviour
 {
-    private int gems = 0;
+    private int gemsSum = 5;
+    private int cherriesSum = 10;
+
+    private int gemsCollected = 0;
+    private int cherriesCollected = 0;
+
+    [SerializeField] private TextMeshProUGUI gemsCounter;
+    [SerializeField] private TextMeshProUGUI cherriesCounter;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Gem"))
         {
             Destroy(collision.gameObject);
-            gems++;
-            Debug.Log("Gems: " +  gems);
+            gemsCollected++;
+            gemsCounter.text = "Gems: " + gemsCollected + "/" + gemsSum;
+        }
+
+        if (collision.gameObject.CompareTag("Cherry"))
+        {
+            Destroy(collision.gameObject);
+            cherriesCollected++;
+            cherriesCounter.text = "Gems: " + cherriesCollected + "/" + cherriesSum;
         }
     }
 }
